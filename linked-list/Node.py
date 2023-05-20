ae7edeca67f5d141e35c826a5f1a51fb51a370aa
AndersonIvanildo <andersonivanildo@protonmail.com>
2023-05-20 17:17:19 -0300

    Update Node.py

--- linked-list/Node.py
+++ linked-list/Node.py
@@ -1,5 +1,6 @@
 # Node class using in files and examples
 
 class Node:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, value=None):
+        self.value = value
+        self.nextNode = None
